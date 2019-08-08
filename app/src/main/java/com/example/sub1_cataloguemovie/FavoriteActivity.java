883b92c6c42be1e1e2245ab45fa8050e2d948e11
Auzan <auzanassidqi@gmail.com>
2019-08-08 23:22:44 +0700

    split favorite for movie and tv show

--- app/src/main/java/com/example/sub1_cataloguemovie/FavoriteActivity.java
+++ app/src/main/java/com/example/sub1_cataloguemovie/FavoriteActivity.java
@@ -1,106 +1,43 @@
 package com.example.sub1_cataloguemovie;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
 
-import com.example.sub1_cataloguemovie.adapter.MovieAdapter;
-import com.example.sub1_cataloguemovie.db.DatabaseClient;
-import com.example.sub1_cataloguemovie.model.Movie;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import io.reactivex.Completable;
-import io.reactivex.CompletableObserver;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Action;
-import io.reactivex.schedulers.Schedulers;
+import com.example.sub1_cataloguemovie.adapter.ViewPagerAdapter;
 
 public class FavoriteActivity extends AppCompatActivity {
-
-    private List<Movie> srcDB;
-    private ArrayList<Movie> movies;
-//    final public static String KEY_MOVIES = "key_movies";
-    private RecyclerView rvCategory;
-    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_favorite);
+        setContentView(R.layout.activity_main);
 
-        initViews();
-        getFav();
-
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-
-                new Handler().postDelayed(new Runnable() {
-                    @Override public void run() {
-                        swipeRefreshLayout.setRefreshing(false);
-                        initViews();
-                        getFav();
-                    }
-                }, 2000);
-            }
-        });
+        initView();
     }
 
-    private void initViews() {
+    public void initView(){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Favorite");
+        setSupportActionBar(toolbar);
         if (getSupportActionBar() != null){
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
-        swipeRefreshLayout = findViewById(R.id.sw_layout);
-        rvCategory = findViewById(R.id.rv_list);
-        rvCategory.setHasFixedSize(true);
-        rvCategory.setLayoutManager(new LinearLayoutManager(this));
+
+        ViewPager viewPager = findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
-    private void getFav(){
-        Completable.fromAction(new Action() {
-            @Override
-            public void run() {
-                srcDB = DatabaseClient.getInstance(getApplicationContext()).getAppDatabase().movieDao().getAll();
-                movies = new ArrayList<>(srcDB);
-                generateMovieList(movies);
-            }
-
-        }).observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe(new CompletableObserver() {
-            @Override
-            public void onSubscribe(Disposable d) {
-
-            }
-
-            @Override
-            public void onComplete() {
-                Log.d("Get Fav", "onComplete: " + movies);
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                Log.d("Get Fav", "onErroe: " + movies);
-            }
-        });
-    }
-
-    private void generateMovieList(ArrayList<Movie> movies) {
-        MovieAdapter adapter = new MovieAdapter(this);
-        adapter.setListMovie(movies);
-        rvCategory.setAdapter(adapter);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        initViews();
-        getFav();
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new FavMovieFragment(), getResources().getString(R.string.movies));
+        adapter.addFragment(new FavTvShowFragment(), getResources().getString(R.string.tv_shows));
+        viewPager.setAdapter(adapter);
     }
 
 }
